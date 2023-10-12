73212bea69df3afc9b3b77c589d4b41847b12c45
AG Tribbs <andrewagrover@gmail.com>
2023-10-12 08:12:05 -0500

    First Commit

---
+++ web-scraping-tool.py
@@ -0,0 +1,21 @@
+###
+#   web-scraping-tool
+#   Project 4 given by the AI overlords to learn Python.
+#   Written on Stream @ twitch.tv/CodeNameTribbs
+#   Requirements:
+#   1. Create an app that can scrape informations from a website of your choice.
+#   2. Use a web scraping library like BeautifulSoup or Scrapy or Mechanize.
+#   3. Define what type of data you want to scrape.
+#   4. Implement error handling to gracefully handle situations like failed requests, missing data,
+#      or changes in the website's structure.
+#   5. Organize the scraped data and display it in a structured format such as a list, table, or CSV file
+#   6. Allow the user to specify the number of pages to scrape (if applicacable) and the data to
+#       extract (e.g. new headlines from different categories)
+#   7. Provide a command line interface that allows the user to input the website URL to specify scraping parameters
+#   
+#   OPTIONAL: Save the scrapped, formatted data to a file.
+#   
+#   CHATTER SUGGEST: Dockerize
+#   CHATTER SUGGEST: TUI - Terminal User Interface
+#   TARGET SITE: https://webscraper.io/test-sites
+
